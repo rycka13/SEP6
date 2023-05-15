7fac484579f119145e3b3bdfe0a8dbc72830cc0b
Ioan Sorin Baicoianu <baicoianuioansorin@gmail.com>
2023-05-15 21:22:10 +0200

    changed mocks according to the interface

--- BestMoviesApplication/src/util/mocks/ratings_mock.ts
+++ BestMoviesApplication/src/util/mocks/ratings_mock.ts
@@ -4,37 +4,37 @@ import {Rating} from "../../model/rating";
 
 export const ratingsMock: Rating[] = [
   {
-    movie: moviesMock[0],
+    movieID: moviesMock[0].id,
     rating: 5.4,
     votes: 12,
   },
   {
-    movie: moviesMock[1],
+    movieID: moviesMock[1].id,
     rating: 6.2,
     votes: 20,
   },
   {
-    movie: moviesMock[2],
+    movieID: moviesMock[2].id,
     rating: 5.6,
     votes: 15,
   },
   {
-    movie: moviesMock[3],
+    movieID: moviesMock[3].id,
     rating: 7.1,
     votes: 8,
   },
   {
-    movie: moviesMock[4],
+    movieID: moviesMock[4].id,
     rating: 6.4,
     votes: 78118,
   },
   {
-    movie: moviesMock[5],
+    movieID: moviesMock[5].id,
     rating: 6.5,
     votes: 258,
   },
   {
-    movie: moviesMock[6],
+    movieID: moviesMock[6].id,
     rating: 7.4,
     votes: 1566
   },
