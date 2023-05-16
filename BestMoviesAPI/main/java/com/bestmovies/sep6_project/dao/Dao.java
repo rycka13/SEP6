b8cd9168abaa1e2ecb0e163e00254e768ce2af2c
Titas212 <45366405+Titas212@users.noreply.github.com>
2023-05-16 20:36:36 +0200

    Made Dao class a singleton and got rid of a lot of boilerplate code.

--- BestMoviesAPI/main/java/com/bestmovies/sep6_project/dao/Dao.java
+++ BestMoviesAPI/main/java/com/bestmovies/sep6_project/dao/Dao.java
@@ -4,10 +4,6 @@ import com.bestmovies.sep6_project.dao.interfaces.IDirectorMapper;
 import com.bestmovies.sep6_project.dao.interfaces.IMovieMapper;
 import com.bestmovies.sep6_project.dao.interfaces.IRatingMapper;
 import com.bestmovies.sep6_project.dao.interfaces.IStarMapper;
-import com.bestmovies.sep6_project.model.Director;
-import com.bestmovies.sep6_project.model.Movie;
-import com.bestmovies.sep6_project.model.Rating;
-import com.bestmovies.sep6_project.model.Star;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -15,92 +11,29 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.Reader;
-import java.util.List;
 
 public class Dao {
+    private static Dao single_instance = null;
+    public SqlSession session;
+    public IMovieMapper movieMapper;
+    public IRatingMapper ratingMapper;
+    public IDirectorMapper directorMapper;
+    public IStarMapper starMapper;
 
-    private IMovieMapper movieMapper;
-    private IRatingMapper ratingMapper;
-    private IDirectorMapper directorMapper;
-    private IStarMapper starMapper;
-    private SqlSession session;
-
-    public Dao() throws IOException {
+    private Dao() throws IOException {
         String resource = "Mapper.xml";
             Reader reader = Resources.getResourceAsReader(resource);
             SqlSessionFactory sqlSessionFactory =
                     new SqlSessionFactoryBuilder().build(reader);
             session = sqlSessionFactory.openSession();
-            movieMapper = session.getMapper(IMovieMapper.class);
-            ratingMapper = session.getMapper(IRatingMapper.class);
-            directorMapper = session.getMapper(IDirectorMapper.class);
-            starMapper = session.getMapper(IStarMapper.class);
+        movieMapper = session.getMapper(IMovieMapper.class);
+        ratingMapper = session.getMapper(IRatingMapper.class);
+        directorMapper = session.getMapper(IDirectorMapper.class);
+        starMapper = session.getMapper(IStarMapper.class);
     }
+    public static synchronized Dao getInstance() throws IOException {
+        if(single_instance == null) single_instance = new Dao();
 
-    //Movies
-    public List<Movie> getAllMovies(){
-        return movieMapper.getAll();
-    }
-
-    //Ratings
-    public List<Rating> getAllRatings(){
-        return ratingMapper.getAll();
-    }
-
-    //Directors
-
-    public List<Director> getAllDirectors(){
-        return directorMapper.getAll();
-    }
-    public List<Star> getAllStars(){
-        return starMapper.getAll();
-    }
-    public void createMovie(Movie movie){
-        movieMapper.createMovie(movie);
-        session.commit();
-    }
-    public void updateMovie(Movie movie){
-        movieMapper.updateMovie(movie);
-        session.commit();
-    }
-    public void deleteMovie(int id){
-        movieMapper.deleteMovie(id);
-        session.commit();
-    }
-    public void createStar(Star star){
-        starMapper.createStar(star);
-        session.commit();
-    }
-    public void updateStar(Star star){
-        starMapper.updateStar(star);
-        session.commit();
-    }
-    public void deleteStar(int id){
-        starMapper.deleteStar(id);
-        session.commit();
-    }
-    public void createDirector(Director director){
-        directorMapper.createDirector(director);
-        session.commit();
-    }
-    public void updateDirector(Director director){
-        directorMapper.updateDirector(director);
-        session.commit();
-    }
-    public void deleteDirector(int id){
-        directorMapper.deleteDirector(id);
-        session.commit();
-    }
-    public void createRating(Rating rating){
-        ratingMapper.createRating(rating);
-        session.commit();
-    }
-    public void updateRating(Rating rating){
-        ratingMapper.updateRating(rating);
-        session.commit();
-    }
-    public void deleteRating(int id){
-        ratingMapper.deleteRating(id);
-        session.commit();
+        return single_instance;
     }
 }
