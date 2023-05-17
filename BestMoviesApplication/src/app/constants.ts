e0dee75c72dadedbdb1d694fe29d0372673dd085
Ioan Sorin Baicoianu <baicoianuioansorin@gmail.com>
2023-05-18 00:42:05 +0200

    added paginate for pagination and more mocks

--- BestMoviesApplication/src/app/constants.ts
+++ BestMoviesApplication/src/app/constants.ts
@@ -117,3 +117,9 @@ export function updateSelectedParentAtIndex(indexOfParent: number, isSelected: b
     return false;
   }
 }
+
+//pagination
+export function paginate(array, page_size, page_number) {
+  return array.slice((page_number - 1) * page_size, page_number * page_size);
+}
+
