b4a4dfe0b3f5fbe8d4b451027e3cd773bbdbf2f2
Titas212 <45366405+Titas212@users.noreply.github.com>
2023-05-19 17:36:09 +0200

    Added @Mapper annotations

--- BestMoviesAPI/main/java/com/bestmovies/sep6_project/dao/interfaces/IRatingMapper.java
+++ BestMoviesAPI/main/java/com/bestmovies/sep6_project/dao/interfaces/IRatingMapper.java
@@ -2,9 +2,10 @@ package com.bestmovies.sep6_project.dao.interfaces;
 
 import com.bestmovies.sep6_project.model.Movie;
 import com.bestmovies.sep6_project.model.Rating;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
-
+@Mapper
 public interface IRatingMapper {
     List<Rating> getAll();
     void createRating(Rating rating);
