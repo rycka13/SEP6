ca31a3ec039bfcb0a120354210f9a68e80a36c66
Ioan Sorin Baicoianu <baicoianuioansorin@gmail.com>
2023-05-20 19:52:51 +0200

    updated component with movies rating

--- BestMoviesApplication/src/app/overall-information/overall-information.component.ts
+++ BestMoviesApplication/src/app/overall-information/overall-information.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
-import {Select, Selector, Store} from "@ngxs/store";
+import {Select, Store} from "@ngxs/store";
 import {OverallInformationSelector} from "./overall-information.selector";
 import {Observable} from "rxjs";
 import {Movie} from "../../model/movie";
@@ -9,15 +9,18 @@ import {
   OverAllInformationFetchInfo,
   OverAllInformationReset
 } from "./overall-information.actions";
-import {CellClickedEvent, ColDef, ColumnApi, GridApi, GridReadyEvent} from "ag-grid-community";
+import {ColumnApi, GridApi} from "ag-grid-community";
 import {AgGridAngular} from "ag-grid-angular";
 import {Router} from "@angular/router";
 import {
   MovieOverviewFetchBestMoviesTop,
-  MovieOverviewFetchMoviesFromSameYear
+  MovieOverviewFetchMoviesFromSameYear,
+  MovieOverviewFetchSameRatingRange
 } from "src/app/information/movies/movie-overview/movies-overview.actions";
 import {MoviesOverviewSelector} from "src/app/information/movies/movie-overview/movies-overview.selector";
 import {NbToastrService} from "@nebular/theme";
+import {randomNumberFromInterval} from "src/util/utils_functions";
+import {LoadEnum, SearchByEnum} from "src/app/overall-information/constants";
 
 @Component({
   selector: 'app-overall-information',
@@ -46,16 +49,28 @@ export class OverallInformationComponent implements OnInit, OnDestroy {
   @Select(MoviesOverviewSelector.topMoviesByYear)
   bestMoviesByYear$: Observable<Movie[]>;
 
+  @Select(MoviesOverviewSelector.topMoviesByRating)
+  bestMoviesByRating$: Observable<Movie[]>;
+
   gridApi: GridApi;
   gridColumnApi: ColumnApi;
   @ViewChild('agGridAngular') agGrid!: AgGridAngular;
 
   TOP_BEST_MOVIES: string = '5';
-  TOP_BEST_MOVIES_YEAR: string = '5';
+
+  //movies by year
+  LIST_MOVIES_YEAR: string = '5';
   YEAR_NOW: number;
   YEAR_SEARCHED: number;
   moviesByYearIsFiltered: boolean = false;
-  placeHolder: string;
+  moviesByYearPlaceHolder: string;
+
+  //movies by rating
+  LIST_MOVIES_RATING: string = '5';
+  RANDOMLY_RATING: number;
+  RATING_SEARCHED: number;
+  moviesByRatingIsFiltered: boolean = false;
+  moviesByRatingPlaceHolder: string;
 
 
   constructor(private store: Store,
@@ -64,54 +79,104 @@ export class OverallInformationComponent implements OnInit, OnDestroy {
   }
 
   async ngOnInit() {
+   //movies by year
     let date = new Date();
     this.YEAR_NOW = date.getUTCFullYear();
     this.YEAR_SEARCHED = this.YEAR_NOW;
-    this.placeHolder = `Year searched ${this.YEAR_SEARCHED}`;
+    this.moviesByYearPlaceHolder = `Year searched ${this.YEAR_SEARCHED}`;
+
+    //movies by rating
+    this.RANDOMLY_RATING = randomNumberFromInterval(0,10, 1);
+    this.RATING_SEARCHED = this.RANDOMLY_RATING;
+    this.moviesByRatingPlaceHolder = `Rating searched ${this.RATING_SEARCHED}`;
     const actionsInParallel = [
       new OverAllInformationFetchInfo(),
       new OverAllInformationBestMoviesFetch(),
       new MovieOverviewFetchBestMoviesTop(Number(this.TOP_BEST_MOVIES)),
-      new MovieOverviewFetchMoviesFromSameYear(Number(this.TOP_BEST_MOVIES_YEAR), this.YEAR_NOW),
+      new MovieOverviewFetchMoviesFromSameYear(Number(this.LIST_MOVIES_YEAR), this.YEAR_NOW),
+      new MovieOverviewFetchSameRatingRange(Number(this.LIST_MOVIES_RATING), this.RATING_SEARCHED)
     ];
     this.store.dispatch([...actionsInParallel]);
   }
 
-  onSearchMoviesByYear(event) {
-    this.YEAR_SEARCHED = Number(event);
-    if (isNaN(this.YEAR_SEARCHED)) {
-      return this.nbToastrService.show(`Searched for ${event}'`, `You only need to include numbers in search`, {
-        status: 'warning'
-      })
+  onSearch(searchBy: SearchByEnum, event) {
+    if(searchBy == SearchByEnum.YEAR) {
+      this.YEAR_SEARCHED = Number(event);
+      if (isNaN(this.YEAR_SEARCHED)) {
+        this.YEAR_SEARCHED = this.YEAR_NOW;
+        return this.nbToastrService.show(`Searched for ${event}'`, `You only need to include numbers in search`, {
+          status: 'warning'
+        })
+      }
+      else {
+        this.moviesByYearIsFiltered = true;
+        this.moviesByYearPlaceHolder = `Year searched ${this.YEAR_SEARCHED}`;
+        this.store.dispatch(new MovieOverviewFetchMoviesFromSameYear(Number(this.LIST_MOVIES_YEAR), this.YEAR_SEARCHED));
+      }
     }
-    else {
-      this.moviesByYearIsFiltered = true;
-      this.placeHolder = `Year searched ${this.YEAR_SEARCHED}`;
-      this.store.dispatch(new MovieOverviewFetchMoviesFromSameYear(Number(this.TOP_BEST_MOVIES_YEAR), this.YEAR_SEARCHED));
+    else if(searchBy == SearchByEnum.RATING) {
+      this.RATING_SEARCHED = Number(event);
+      if (isNaN(this.RATING_SEARCHED)) {
+        this.RATING_SEARCHED = this.RANDOMLY_RATING;
+        return this.nbToastrService.show(`Searched for ${event}'`, `You only need to include numbers in search`, {
+          status: 'warning'
+        })
+      }
+      else {
+        this.moviesByRatingIsFiltered = true;
+        this.moviesByRatingPlaceHolder = `Rating searched ${this.RATING_SEARCHED}`;
+        this.store.dispatch(new MovieOverviewFetchSameRatingRange(Number(this.LIST_MOVIES_RATING), this.RATING_SEARCHED));
+      }
     }
   }
 
-  resetSearchMoviesByYear() {
-    this.moviesByYearIsFiltered = false;
-    this.YEAR_SEARCHED = this.YEAR_NOW;
-    this.placeHolder = `Year searched ${this.YEAR_SEARCHED}`;
-    this.store.dispatch(new MovieOverviewFetchMoviesFromSameYear(Number(this.TOP_BEST_MOVIES_YEAR), this.YEAR_NOW));
+  resetSearch(searchBy: SearchByEnum) {
+    if(searchBy == SearchByEnum.YEAR) {
+      this.moviesByYearIsFiltered = false;
+      this.YEAR_SEARCHED = this.YEAR_NOW;
+      this.moviesByYearPlaceHolder = `Year searched ${this.YEAR_SEARCHED}`;
+      this.store.dispatch(new MovieOverviewFetchMoviesFromSameYear(Number(this.LIST_MOVIES_YEAR), this.YEAR_NOW));
+    }
+    else if(searchBy == SearchByEnum.RATING) {
+      this.moviesByRatingIsFiltered = false;
+      this.RATING_SEARCHED = this.RANDOMLY_RATING;
+      this.moviesByRatingPlaceHolder = `Rating searched ${this.RANDOMLY_RATING}`;
+      this.store.dispatch(new MovieOverviewFetchSameRatingRange(Number(this.LIST_MOVIES_RATING), this.RANDOMLY_RATING));
+    }
   }
 
   redirectToMovieOverviewPage(movieId: number) {
     this.router.navigate([`/information/movies/${movieId}`]);
   }
 
-  loadBestMovies() {
-    this.store.dispatch(new MovieOverviewFetchBestMoviesTop(Number(this.TOP_BEST_MOVIES)));
-  }
-
-  loadBestMoviesByYear() {
-    this.store.dispatch(new MovieOverviewFetchMoviesFromSameYear(Number(this.TOP_BEST_MOVIES_YEAR), this.YEAR_NOW));
+  loadMovies(loadBy: LoadEnum) {
+    switch (loadBy) {
+      case LoadEnum.BEST_MOVIES: {
+        this.store.dispatch(new MovieOverviewFetchBestMoviesTop(Number(this.TOP_BEST_MOVIES)));
+        break;
+      }
+      case LoadEnum.MOVIES_BY_YEAR: {
+        this.store.dispatch(new MovieOverviewFetchMoviesFromSameYear(Number(this.LIST_MOVIES_YEAR), this.YEAR_NOW));
+        break;
+      }
+      case LoadEnum.MOVIES_BY_RATING: {
+        this.store.dispatch(new MovieOverviewFetchSameRatingRange(Number(this.LIST_MOVIES_YEAR), this.RANDOMLY_RATING));
+        break;
+      }
+    }
   }
 
   ngOnDestroy() {
     this.alive = false;
     this.store.dispatch(new OverAllInformationReset());
+  }
+
+  //enums
+  getSearchByEnum() : typeof SearchByEnum {
+    return SearchByEnum;
+  }
+
+  getLoadEnum() : typeof LoadEnum {
+    return LoadEnum;
   }
 }
