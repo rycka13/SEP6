f138069bc77d999eac6c0aca5bf65d48c0d3843f
rycka13 <71268611+rycka13@users.noreply.github.com>
2023-05-21 18:05:20 +0300

    Updated rest client and updated services
    Now Movies come with description,
    stars and directors have profile pictures in the object

---
+++ BestMoviesAPI/main/java/com/bestmovies/sep6_project/restclient/Key.java
@@ -0,0 +1,22 @@
+package com.bestmovies.sep6_project.restclient;
+
+public class Key {
+    private String api_key;
+    private String api_bearer;
+
+    public String getApi_key() {
+        return api_key;
+    }
+
+    public void setApi_key(String api_key) {
+        this.api_key = api_key;
+    }
+
+    public String getApi_bearer() {
+        return api_bearer;
+    }
+
+    public void setApi_bearer(String api_bearer) {
+        this.api_bearer = api_bearer;
+    }
+}
