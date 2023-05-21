aa8179862ff338212e96c3cb605e47d232b41870
Ioan Sorin Baicoianu <baicoianuioansorin@gmail.com>
2023-05-21 23:13:02 +0200

    added new variables from BE for person and deleted the variables from director and stars since is extended

--- BestMoviesApplication/src/model/director.ts
+++ BestMoviesApplication/src/model/director.ts
@@ -2,9 +2,4 @@ import {Movie} from "./movie";
 import {Person} from "src/model/person";
 
 export interface Director extends Person {
-  movies: Movie[];
-  id: number;
-  name: string;
-  birthYear: number;
-  posterImage: string;
 }
--- BestMoviesApplication/src/model/person.ts
+++ BestMoviesApplication/src/model/person.ts
@@ -1,9 +1,11 @@
 import {Movie} from "src/model/movie";
 
 export interface Person {
+
+  addMovie: Movie;
   movies: Movie[];
   id: number;
   name: string;
   birthYear: number;
-  posterImage: string;
+  profilePicture: string;
 }
