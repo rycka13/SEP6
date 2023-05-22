c041736222c4643c20e56e80e378a44afb5206e3
Ioan <ioan.baicoianu@se.com>
2023-05-22 22:51:27 +0200

    updated spec class so tests are not failing

--- BestMoviesApplication/src/core/services/jwt.service.spec.ts
+++ BestMoviesApplication/src/core/services/jwt.service.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed } from '@angular/core/testing';
-
-import { JwtService } from './jwt.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Store } from "@ngxs/store";
+import { PROVIDERS } from "../../app/app.module";
+import { JwtService } from "./jwt.service";
 
 describe('JwtService', () => {
   let service: JwtService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
+      declarations: [],
+      providers: [
+        ...PROVIDERS,
+        Store,
+      ],
+    });
     service = TestBed.inject(JwtService);
   });
 
