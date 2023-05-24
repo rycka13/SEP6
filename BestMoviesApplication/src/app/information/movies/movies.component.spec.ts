b4962a2302402bfe899f8c5cbe494d94af8af56d
Ioan <ioan.baicoianu@se.com>
2023-05-24 22:29:43 +0200

    updated test so it's not using any real api on service

--- BestMoviesApplication/src/app/information/movies/movies.component.spec.ts
+++ BestMoviesApplication/src/app/information/movies/movies.component.spec.ts
@@ -1,60 +1,46 @@
-import {Spectator} from "@ngneat/spectator";
-import {createComponentFactory, SpectatorFactory} from "@ngneat/spectator/jest";
-import {NgxsModule, Store} from "@ngxs/store";
-import {MoviesComponent} from "./movies.component";
-import {RouterTestingModule} from "@angular/router/testing";
-import {defaultsState, MoviesState} from "./movies.state";
-import {BrowserModule} from "@angular/platform-browser";
-import {AppRoutingModule} from "../../app-routing.module";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {NEBULAR_MODULES, PROVIDERS, STATES} from "../../app.module";
-import {HttpClientModule} from "@angular/common/http";
-import {environment} from "../../../environments/environment";
+import { Spectator } from "@ngneat/spectator";
+import { createComponentFactory } from "@ngneat/spectator/jest";
+import { NgxsModule, Store } from "@ngxs/store";
+import { MoviesComponent } from "./movies.component";
+import { RouterTestingModule } from "@angular/router/testing";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "../../app-routing.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NEBULAR_MODULES, PROVIDERS, STATES } from "../../app.module";
+import { HttpClientModule } from "@angular/common/http";
+import { of } from "rxjs";
+import { MovieService } from "../../../api/movie.service";
 
+const movieServiceMock = {
+  getMovies: jest.fn().mockReturnValue(of([])),
+};
 
 describe('app > information > movies > movies.component.spec.ts', () => {
-  let store: Store;
-  let component: MoviesComponent;
   let spectator: Spectator<MoviesComponent>;
-  const createComponent: SpectatorFactory<MoviesComponent> =
-    createComponentFactory({
-      component: MoviesComponent,
-      imports: [
-        RouterTestingModule,
-        BrowserModule,
-        HttpClientModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        NgxsModule.forRoot(STATES, {
-          developmentMode: !environment.production,
-        }),
-        ...NEBULAR_MODULES,
-      ],
-      declarations: [],
-      providers: [
-        ...PROVIDERS,
-        Store,
-      ],
-    });
 
-  beforeEach(() => {
-    spectator = createComponent({
-      detectChanges: false,
-    });
-
-    store = spectator.inject(Store);
-
-    store.reset({
-      moviesPage: defaultsState,
-    })
+  const createComponent = createComponentFactory({
+    component: MoviesComponent,
+    imports: [
+      RouterTestingModule,
+      BrowserModule,
+      HttpClientModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      NgxsModule.forRoot(STATES),
+      ...NEBULAR_MODULES,
+    ],
+    providers: [
+      ...PROVIDERS,
+      Store,
+      { provide: MovieService, useValue: movieServiceMock },
+    ],
   });
 
-  it('should create', async () => {
-    spectator.detectChanges();
-    await spectator.fixture.whenStable();
-    spectator.detectChanges();
-    await spectator.fixture.whenRenderingDone();
+  beforeEach(() => {
+    spectator = createComponent();
+  });
 
+  it('should create', () => {
     expect(spectator.component).toBeTruthy();
   });
 
