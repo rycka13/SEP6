c997266e1bde802dac3fa94a430e3186dd374ae6
Ioan <ioan.baicoianu@se.com>
2023-05-24 20:19:11 +0200

    added top-list into the menu items

--- BestMoviesApplication/src/app/constants.ts
+++ BestMoviesApplication/src/app/constants.ts
@@ -6,12 +6,18 @@ import {NbMenuItem} from "@nebular/theme";
 
 export const PARENT_IDS = {
   OVERALL_INFORMATION_ID: 'overall-information',
+  USER_LIST_ID: 'user-list',
   INFORMATION_ID: 'information',
 }
 export const PAGE_GENERAL_TITLES = {
   //GENERAL MENU
   OVERALL: 'Overall',
   INFORMATION: 'Information'
+}
+
+export const PAGE_USER_LIST_TITLES = {
+  //GENERAL MENU
+  TOP: 'Top movies',
 }
 
 export const PAGE_INFORMATION_TITLES = {
@@ -40,6 +46,17 @@ export const INFORMATION_MENU_ITEMS: NbMenuItem[] = [
   }
 ];
 
+export const USER_LIST_MENU_ITEMS: NbMenuItem[] = [
+  {
+    title: PAGE_USER_LIST_TITLES.TOP,
+    link: 'user-list/top',
+    icon: '',
+    data: {
+      id: 'user-top-list'
+    }
+  }
+];
+
 export const GENERAL_MENU_ITEMS: NbMenuItem[] = [
   {
     title: PAGE_GENERAL_TITLES.OVERALL,
@@ -48,6 +65,14 @@ export const GENERAL_MENU_ITEMS: NbMenuItem[] = [
     data: {
       id: PARENT_IDS.OVERALL_INFORMATION_ID
     }
+  },
+  {
+    title: 'My list of movies ',
+    children: USER_LIST_MENU_ITEMS,
+    expanded: true,
+    data: {
+      id: PARENT_IDS.USER_LIST_ID,
+    },
   },
   {
     title: PAGE_GENERAL_TITLES.INFORMATION,
--- BestMoviesApplication/src/app/user-list/user-list-routing.module.ts
+++ BestMoviesApplication/src/app/user-list/user-list-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserListComponent } from './user-list.component';
+import { TopListComponent } from "src/app/user-list/top-list/top-list.component";
 
-const routes: Routes = [{ path: '', component: UserListComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: UserListComponent
+  },
+  {
+    path:'top',
+    component: TopListComponent,
+  }
+  ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
