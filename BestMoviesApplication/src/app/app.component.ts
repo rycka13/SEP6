9ebc3b281359f086a5b06800e4f537735ae43996
Ioan <ioan.baicoianu@se.com>
2023-05-24 16:57:11 +0200

    added authservice to check if user is logged in and added buttons in the header

--- BestMoviesApplication/src/app/app.component.ts
+++ BestMoviesApplication/src/app/app.component.ts
@@ -6,23 +6,48 @@ import {
   updateSelectedChildAtIndexOfParent, updateSelectedParentAtIndex
 } from "src/core/helpers/helpers";
 import {GENERAL_MENU_ITEMS, PARENT_IDS} from "src/app/constants";
+import { AuthService } from "src/core/services/auth.service";
 
 @Component({
   selector: 'app-root',
   template: `
     <nb-layout>
       <nb-layout-header fixed>
-        <button
-          nbButton
-          ghost
-          shape="round"
-          (click)="toggle()"
-        >
-          <nb-icon [icon]="isToggled ? 'arrowhead-right-outline' : 'arrowhead-left-outline'"></nb-icon>
-        </button>
+        <div class="left-container">
+          <button
+            nbButton
+            ghost
+            shape="round"
+            (click)="toggle()"
+          >
+            <nb-icon [icon]="isToggled ? 'arrowhead-right-outline' : 'arrowhead-left-outline'"></nb-icon>
+          </button>
+        </div>
+        <div class="right-container">
+          <button
+            *ngIf="!isLoggedIn"
+            nbButton
+            ghost
+            shape="round"
+            (click)="toggle()"
+            nbTooltip="Log in"
+          >
+            <nb-icon icon="log-in-outline"></nb-icon>
+          </button>
+          <button
+            *ngIf="isLoggedIn"
+            nbButton
+            ghost
+            shape="round"
+            nbTooltip="Log out"
+            (click)="toggle()"
+          >
+            <nb-icon icon="log-out-outline"></nb-icon>
+          </button>
+        </div>
         <a href="#" (click)="toggle()"><i class="nb-menu"></i></a>
       </nb-layout-header>
-      <nb-sidebar>
+      <nb-sidebar *ngIf="isLoggedIn">
         <nb-menu [items]="GENERAL_MENU_ITEMS"></nb-menu>
       </nb-sidebar>
       <nb-layout-column class="colored-column-basic">
@@ -36,8 +61,13 @@ export class AppComponent {
   GENERAL_MENU_ITEMS = GENERAL_MENU_ITEMS;
   selectedItem: any;
   isToggled = false;
+  isLoggedIn: boolean = false;
   constructor(private sideBarService: NbSidebarService,
-              private menu: NbMenuService) {
+              private menu: NbMenuService,
+              public authService: AuthService) {
+
+    // TODO this.isLoggedIn = authService.hasLoggedIn;
+    this.isLoggedIn = true;
 
     let indexOfItem = getIndexOfParent(PARENT_IDS.OVERALL_INFORMATION_ID);
     this.selectedItem = GENERAL_MENU_ITEMS[indexOfItem];
